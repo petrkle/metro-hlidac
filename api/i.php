deecf3d4a45c6e36d2389ede2fab86111e4618b1
Petr <petr@kle.cz>
2017-04-21 18:35:27 +0200

    Větší obrázek

--- api/i.php
+++ api/i.php
@@ -20,7 +20,7 @@ $image['title']=extract_text($dom,"//div[@class='text']/h3");
 $image['text']=extract_text($dom,"//div[@class='text']/p");
 $image['authors']=extract_text($dom,"//span[@class='autor']");
 $image['time']=extract_text($dom,"//span[@class='time']");
-$image['img']='http:'.extract_atr($dom,"//td[@class='equ-img']/img","src");
+$image['img']=preg_replace('/\/cl6h\//', '/org/', 'http:'.extract_atr($dom,"//td[@class='equ-img']/img","src"));
 $image['prev']=preg_replace('/.*=/','',extract_atr($dom,"//a[@class='img-prev ']","href"));
 $image['next']=preg_replace('/.*=/','',extract_atr($dom,"//a[@class='img-next ']","href"));
 $image=json_encode($image);
